1a95e1f42813d54d38817cdb3bae1eb6840f1573
Carlos Massavanhane <cmassavanhanejr@gmail.com>
2020-03-30 18:36:12 +0200

    Adding: outros relacionamentos familiares
    avó. avô, mulher e tio

--- familyRelationShip.pl
+++ familyRelationShip.pl
@@ -37,6 +37,11 @@ clauses
 	mae(X,Y) :- gerou(X,Y), feminino(X).
 	pai(X,Y) :- gerou(X,Y), masculino(X).
 	irma(X,Y) :- gerou(Z,X), gerou(Z,Y), feminino(X),X\==Y.
+	antepassado(X,Y) :- gerou(X,Z), gerou(Z,Y).
+	avoh(X,Z) :- mae(X,Y), gerou(Y,Z).
+	avo(X,Z) :- pai(X,Y), gerou(Y,Z).
+	mulher(X,Y) :- gerou(X,Z), gerou(Y,Z), feminino(X), masculino(Y).
+	tio(X,Z) :- irmao(X,Y), gerou(Y,Z).
 	temFilho(X):- gerou(X,_).
 	% _ variavel anonima
 	irmao(X,Y) :- gerou(Z,X), gerou(Z,Y), masculino(X),X\==Y.
